339e3db2a9df88f7d89a4a332feb7e1367666111
Niyant Shah <niyantshah02@gmail.com>
2020-04-12 04:07:53 +0530

    created Nodejs Project with todo rest api and swagger.

---
+++ src/configs/database.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
